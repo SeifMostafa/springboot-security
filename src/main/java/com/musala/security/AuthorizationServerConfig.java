6e56d412c361ad4a005f3844ded99e16c0fed0db
csseifms <csseifms@gmail.com>
2021-12-14 16:08:56 +0200

    working with product project for assignment

--- src/main/java/com/musala/security/AuthorizationServerConfig.java
+++ src/main/java/com/musala/security/AuthorizationServerConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -23,7 +24,7 @@ import java.security.KeyPair;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-    public static final String RESOURCE_ID = "couponservice";
+    public static final String RESOURCE_ID = "Product";
 
     @Autowired
     AuthenticationManager authenticationManager;
@@ -51,9 +52,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .userDetailsService(userDetailsService);
     }
 
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999")).authorizedGrantTypes("password", "refresh_token")
+//                .scopes("read", "write").resourceIds(RESOURCE_ID);
+//    }
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("couponclientapp").secret(passwordEncoder.encode("9999")).authorizedGrantTypes("password", "refresh_token")
+        clients.inMemory().withClient("productclientapp").secret(passwordEncoder.encode("9999")).authorizedGrantTypes("password", "refresh_token")
                 .scopes("read", "write").resourceIds(RESOURCE_ID);
     }
 
@@ -63,7 +69,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource(keyFile), password.toCharArray());
         KeyPair keyPair = keyStoreKeyFactory.getKeyPair(alias);
         jwtAccessTokenConverter.setKeyPair(keyPair);
+       // jwtAccessTokenConverter.setSigningKey("testkey");
         return jwtAccessTokenConverter;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()");
     }
 
     @Bean
